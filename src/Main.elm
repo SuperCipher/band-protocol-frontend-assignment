99be8dff0effc00f22a6a3ac387c20d8028fc6cf
nach <napat_pat3@hotmail.com>
2022-07-19 21:10:22 +0700

    feat: table view

--- src/Main.elm
+++ src/Main.elm
@@ -2,15 +2,22 @@ port module Main exposing (..)
 
 import Browser
 import Browser.Navigation
-import Html exposing (Html, button, div, text, p)
-import Html.Attributes as Attr
+import Html exposing (Html, button, div, img, p, table, td, text, th, tr)
+import Html.Attributes as Attr exposing (src)
 import Html.Events exposing (onClick)
 import Json.Decode as Decode exposing (Decoder, decodeString, float, int, list, string)
 import Json.Decode.Pipeline exposing (required)
 import Url
 
 
-offset = 3
+
+-- TODO set by user
+
+
+offset =
+    3
+
+
 
 -- PORT
 
@@ -54,6 +61,9 @@ subscriptions _ =
 
 type alias User =
     { id : String
+    , username : String
+    , joined_date : String
+    , profile_image_hash : String
     }
 
 
@@ -61,6 +71,9 @@ userDecoder : Decode.Decoder User
 userDecoder =
     Decode.succeed User
         |> required "id" string
+        |> required "username" string
+        |> required "joined_date" string
+        |> required "profile_image_hash" string
 
 
 
@@ -110,23 +123,69 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         NextPageRecieved receivedUsers ->
-            ( { model | users = receivedUsers}, Cmd.none )
+            ( { model | users = receivedUsers }, Cmd.none )
 
         NextPage currentPage ->
-          let
-            newPage = currentPage + 1
-            newPageOffset = newPage * offset
-          in
-            ( { model | currentPage = newPage }, requestPage (newPageOffset) )
+            let
+                newPage =
+                    currentPage + 1
+
+                newPageOffset =
+                    newPage * offset
+            in
+            ( { model | currentPage = newPage }, requestPage newPageOffset )
+
         PrevPage currentPage ->
-          let
-            newPage =   if currentPage - 1 < 0 then 0 else currentPage - 1
-            newPageOffset = newPage * offset
-          in
-            ( { model | currentPage = newPage }, requestPage (newPageOffset) )
+            let
+                newPage =
+                    if currentPage - 1 < 0 then
+                        0
+
+                    else
+                        currentPage - 1
+
+                newPageOffset =
+                    newPage * offset
+            in
+            ( { model | currentPage = newPage }, requestPage newPageOffset )
 
         _ ->
             ( model, Cmd.none )
+
+
+
+-- TODO refactor UI
+
+
+tableView : List User -> Html msg
+tableView users =
+    let
+        headerRow =
+            tr []
+                [ th [] [ text "id" ]
+                , th [] [ text "username" ]
+                , th [] [ text "joined date" ]
+                , th [] [ text "profile image" ]
+                ]
+
+        userRows =
+            List.map
+                (\user ->
+                    tr []
+                        [ td [ Attr.class "w-40" ] [ text user.id ]
+                        , td [ Attr.class "w-40" ] [ text user.username ]
+
+                        -- Date human readable format
+                        , td [ Attr.class "w-40" ] [ text user.joined_date ]
+                        , td [] [ img [ src ("https://" ++ "api.lorem.space" ++ "/" ++ "image/face" ++ "?w=150&h=150&hash=" ++ user.profile_image_hash) ] [] ]
+                        ]
+                )
+                users
+    in
+    table []
+        (List.append [ headerRow ]
+            userRows
+        )
 
 
 
@@ -139,11 +198,12 @@ view model =
     , body =
         [ div [ Attr.class "h-full min-h-screen flex flex-col" ]
             [ div [ Attr.class "relative max-w-7xl mx-auto px-4 focus:outline-none sm:px-3 md:px-5" ]
-                [ button [ Attr.class "bg-blue-300" ] [ text "Delete all" ]
-                , p [] [ text <| String.fromInt model.currentPage]
-                , div [] <| List.map (\user -> div [] [ text user.id ]) model.users
-                , button [ onClick (PrevPage model.currentPage) ] [ text "previous page" ]
-                , button [ onClick (NextPage model.currentPage) ] [ text "next page" ]
+                [ -- button [ Attr.class "bg-red-500" ] [ text "Delete all" ],
+                  p [] [ text <| (String.fromInt model.currentPage ++ " page number") ]
+                , p [] [ text "Sort by joined date , 3 user each page" ]
+                , tableView model.users
+                , button [ Attr.class "bg-yellow-400", onClick (PrevPage model.currentPage) ] [ text "previous page" ]
+                , button [ Attr.class "bg-green-400", onClick (NextPage model.currentPage) ] [ text "next page" ]
                 ]
             ]
         ]
--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ import { useSubscription, gql } from "@apollo/client";
 
 import { SubscriptionClient } from 'subscriptions-transport-ws';
 
-
+// Move connection stuff to new file
 // Should be in database too
 const httpLink = new HttpLink({
   uri: "http://green-feather-500032.ap-south-1.aws.cloud.dgraph.io/graphql", // Or your Slash GraphQL endpoint (if you're using Slash GraphQL)
@@ -54,41 +54,7 @@ const client = new ApolloClient({
 // TODO set by UI
 const OFFSET: number = 3
 
-async function fetchGraphQL(operationsDoc: any, operationName: any, variables: any) {
-  // TODO: environment variables this
-  const result = await fetch(
-    "https://green-feather-500032.ap-south-1.aws.cloud.dgraph.io/graphql",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "X-Auth-Token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJzL3Byb3h5IiwiZHVpZCI6IjB4M2NhMmI5YyIsImV4cCI6MTY1ODA1MDYwNiwiaXNzIjoicy9hcGkifQ.8zgxEzFduQmyw3C2hI9Te3gId3Ki5HybfmmzzXQqSdo"
-      },
-      body: JSON.stringify({
-        query: operationsDoc,
-        variables: variables,
-        operationName: operationName
-      })
-    }
-  );
-
-  return await result.json();
-}
-
 // TODO refactor this
-function fetchMyQuery(first: number, offset: number, sort: string = "asc", sortBy: string = "joined_date") {
-  return fetchGraphQL(
-    `
-      query ListUser {
-        queryUser(order: {${sort}: ${sortBy}}, first: ${first}, offset: ${offset}) {
-          id
-        }
-      }
-    `,
-    "ListUser",
-    {}
-  );
-}
 
 function subscribeQuery(executeAfter: Function , first: number, offset: number, sort: string = "asc", sortBy: string = "joined_date") {
   this.subscriptionObserver = client.subscribe({
@@ -96,15 +62,18 @@ function subscribeQuery(executeAfter: Function , first: number, offset: number, 
           subscription userSubscribed {
             queryUser(order: {${sort}: ${sortBy}}, first: ${first}, offset: ${offset}) {
               id
+              joined_date
+              profile_image_hash
+              username
             }
           }
         `,
     variables: {}
   }).subscribe({
     next(queriedData) {
+    // NOTE Will leave console.log here for inspection
     console.log('next DATA')
-
-    console.log('DATA', queriedData)
+    console.log('subscribed DATA', queriedData)
     executeAfter(queriedData.data.queryUser)
       // ... call updateQuery to integrate the new comment
       // into the existing list of comments
