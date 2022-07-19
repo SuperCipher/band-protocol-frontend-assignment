598e6cafc9bcb6f9748c06dda712e329ba0d35ca
nach <napat_pat3@hotmail.com>
2022-07-19 14:37:29 +0700

    feat: try to use apollo with elm unsuccessful

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,50 @@
 import { Elm } from "./Main.elm";
+import React from 'react';
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  ApolloProvider,
+  split,
+} from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { WebSocketLink } from "@apollo/link-ws";
 
-// TODO environment variables
+import { useSubscription, gql } from "@apollo/client";
+
+const httpLink = new HttpLink({
+  uri: "http://green-feather-500032.ap-south-1.aws.cloud.dgraph.io/graphql", // Or your Slash GraphQL endpoint (if you're using Slash GraphQL)
+});
+
+const wsLink = process.browser
+  ? new WebSocketLink({
+      uri: `wss://green-feather-500032.ap-south-1.aws.cloud.dgraph.io/graphql`, // Can test with your Slash GraphQL endpoint (if you're using Slash GraphQL)
+      options: {
+        reconnect: true,
+      },
+    })
+  : null;
+
+const splitLink = process.browser
+  ? split(
+      ({ query }) => {
+        const definition = getMainDefinition(query);
+        return (
+          definition.kind === "OperationDefinition" &&
+          definition.operation === "subscription"
+        );
+      },
+      wsLink,
+      httpLink
+    )
+  : httpLink;
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: splitLink,
+});
+
+// TODO set by UI
 const OFFSET: number = 3
 
 async function fetchGraphQL(operationsDoc: any, operationName: any, variables: any) {
@@ -24,6 +68,7 @@ async function fetchGraphQL(operationsDoc: any, operationName: any, variables: a
   return await result.json();
 }
 
+// TODO refactor this
 function fetchMyQuery(first: number, offset: number, sort: string = "asc", sortBy: string = "joined_date") {
   return fetchGraphQL(
     `
@@ -38,10 +83,27 @@ function fetchMyQuery(first: number, offset: number, sort: string = "asc", sortB
   );
 }
 
+function subscribeQuery(first: number, offset: number, sort: string = "asc", sortBy: string = "joined_date") {
+  return useSubscription(
+    `
+      subscription {
+        queryUser(order: {${sort}: ${sortBy}}, first: ${first}, offset: ${offset}) {
+          id
+        }
+      }
+    `
+  );
+}
+
 var elm = Elm.Main.init({ node: document.querySelector("main"), flags: {} });
 
-
+function subscribe(){
+  const { loading, error, data } = subscribeQuery();
+  return { loading, error, data }
+}
 async function main() {
+  console.log('SUBSCRIBE', subscribe())
+
   fetchMyQuery(3, 0).then((fullfilled) => {
     console.log('FULLFILLED', fullfilled.data.queryUser)
     elm.ports.recievedPage.send(fullfilled.data.queryUser);
